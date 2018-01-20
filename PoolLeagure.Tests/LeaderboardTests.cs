d944408b8db1ea0d7155116d8ab61e5fdadde5d8
Rick Bolton <rick.bolton@gmail.com>
2018-01-20 18:17:55 +0000

    Leaderboard challenge logic

--- PoolLeagure.Tests/LeaderboardTests.cs
+++ PoolLeagure.Tests/LeaderboardTests.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace PoolLeagure.Tests
@@ -14,7 +14,10 @@ namespace PoolLeagure.Tests
             var player = new Player("Barry");
             var leaderboard = new Leaderboard();
             var result = leaderboard.JoinPlayer(player);
-            Assert.Contains(player, result);
+            result.ShouldBeEquivalentTo(new[]
+            {
+                player
+            });
         }
 
         [Fact]
@@ -24,8 +27,44 @@ namespace PoolLeagure.Tests
             var player2 = new Player("Larry");
             var leaderboard = new Leaderboard();
             var result = leaderboard.JoinPlayer(player).JoinPlayer(player2);
-            Assert.Contains(player, result);
-            Assert.Equal(player2, result.Last());
+            result.ShouldBeEquivalentTo(new[]
+            {
+                player,
+                player2
+            });
+        }
+
+        [Fact]
+        public void Players_swap_position_when_challenge_won()
+        {
+            var player = new Player("Barry");
+            var player2 = new Player("Larry");
+            var player3 = new Player("Harry");
+            var leaderboard = new Leaderboard();
+            var result = leaderboard.JoinPlayer(player)
+                                    .JoinPlayer(player2)
+                                    .JoinPlayer(player3)
+                                    .WinChallenge(player, player2);
+
+            result.ShouldBeEquivalentTo(new []
+            {
+                player2,
+                player,
+                player3
+            });
+        }
+
+        [Fact]
+        public void Player_can_challenge_up_to_two_rankings_up()
+        {
+            var leaderboard = new Leaderboard().JoinPlayer(new Player("Hester")).JoinPlayer(new Player("Chester"))
+                .JoinPlayer(new Player("Lester")).JoinPlayer(new Player("Nestor"));
+
+            leaderboard.WhoCanIChallenge(new Player("Nestor")).ShouldBeEquivalentTo(new []
+            {
+                new Player("Chester"),
+                new Player("Lester")
+            });
         }
     }
 
@@ -47,6 +86,19 @@ namespace PoolLeagure.Tests
         {
             return GetEnumerator();
         }
+
+        public Leaderboard WinChallenge(Player challengee, Player challenger)
+        {
+            return new Leaderboard()
+            {
+                _ranking = _ranking.Select(x => Equals(x, challengee) ? challenger : (Equals(x, challenger) ? challengee : x))
+            };
+        }
+
+        public IEnumerable<Player> WhoCanIChallenge(Player player)
+        {
+            return _ranking.TakeWhile(x => !x.Equals(player)).Reverse().Take(2);
+        }
     }
 
     public class Player
@@ -56,6 +108,24 @@ namespace PoolLeagure.Tests
         public Player(string name)
         {
             Name = name;
+        }
+
+        protected bool Equals(Player other)
+        {
+            return string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Player) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
         }
     }
 }
